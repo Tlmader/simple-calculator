f4268dd389f5d0ac265b792cad9da222269577cf
Ted Mader <tlmader@live.com>
2015-01-29 00:09:36 -0600

    Updated formatting

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -6,71 +6,71 @@
 
 import java.util.Scanner;
  
-public class SimpleCalculator{
+public class SimpleCalculator {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
 	
-	Scanner input = new Scanner( System.in );
+	Scanner input = new Scanner(System.in);
 	
-	int operation; //Integers 1-4 represent an operation
+	int operation; // Integers 1-4 represent an operation
 	double firstNumber;
 	double secondNumber;
 	double result;
 {
-		System.out.print( "Choose an operation:\n1-Addition\n2-Subtraction\n3-Multiplication\n4-Division\n" );
+		System.out.print("Choose an operation:\n1-Addition\n2-Subtraction\n3-Multiplication\n4-Division\n");
 		operation = input.nextInt();
 	
-	if ( operation == 1 ){ //Addition
-		System.out.print( "Enter first number: ");
+	if (operation == 1) { // Addition
+		System.out.print("Enter first number: ");
 		firstNumber = input.nextDouble();
 
-		System.out.print( "Enter second number: ");
+		System.out.print("Enter second number: ");
 		secondNumber = input.nextDouble();
 
 		result = firstNumber + secondNumber;
 
-		System.out.print( "The sum is " + result );
+		System.out.print("The sum is " + result);
 	
-	}else if ( operation == 2 ){ //Subtraction
+	} else if (operation == 2) { // Subtraction
+
+		System.out.print("Enter first number: ");
+		firstNumber = input.nextDouble();
+
+		System.out.print("Enter second number: ");
+		secondNumber = input.nextDouble();
+
+		result = firstNumber - secondNumber;
+
+		System.out.print("The subtrahend is " + result);
 	
-			System.out.print( "Enter first number: ");
-			firstNumber = input.nextDouble();
+	} else if (operation == 3) { // Multiplication
+	
+		System.out.print("Enter first number: ");
+		firstNumber = input.nextDouble();
 
-			System.out.print( "Enter second number: ");
-			secondNumber = input.nextDouble();
+		System.out.print("Enter second number: ");
+		secondNumber = input.nextDouble();
 
-			result = firstNumber - secondNumber;
+		result = firstNumber * secondNumber;
 
-			System.out.print( "The subtrahend is " + result );
+		System.out.print("The product is " + result);
+	
+	} else if (operation == 4) { // Division
+	
+		System.out.print("Enter first number: ");
+		firstNumber = input.nextDouble();
+
+		System.out.print("Enter second number: ");
+		secondNumber = input.nextDouble();
+
+		result = firstNumber / secondNumber;
+
+		System.out.print("The quotient is " + result);
 		
-		}else if ( operation == 3 ){ //Multiplication
-			
-				System.out.print( "Enter first number: ");
-				firstNumber = input.nextDouble();
-
-				System.out.print( "Enter second number: ");
-				secondNumber = input.nextDouble();
-
-				result = firstNumber * secondNumber;
-
-				System.out.print( "The product is " + result );
-			
-			}else if ( operation == 4 ){ //Division
+	} else // My own personal touch
 				
-					System.out.print( "Enter first number: ");
-					firstNumber = input.nextDouble();
-
-					System.out.print( "Enter second number: ");
-					secondNumber = input.nextDouble();
-
-					result = firstNumber / secondNumber;
-
-					System.out.print( "The quotient is " + result );
-					
-				}else //My own personal touch
-				
-					System.out.print( "You're doing it wrong." );
+		System.out.print("You're doing it wrong.");
 	}
 
   }
